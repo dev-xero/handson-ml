1701a7fe7f322f94ffce80b09bfc3aab621f319e
= <ssh.xero@gmail.com>
2025-07-07 20:38:10 +0100

    feat: load csv and get insights
    Signed-off-by: = <ssh.xero@gmail.com>

--- ch-02-end-to-end-ml-project/utils/data.py
+++ ch-02-end-to-end-ml-project/utils/data.py
@@ -1,5 +1,7 @@
 import os
 import tarfile
+import pandas as pd
+
 from urllib import request
 from pathlib import Path
 
@@ -14,3 +16,8 @@ def download_housing_data(housing_url, housing_path=EXTRACT_PATH):
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
+
+
+def load_housing_data(housing_path=EXTRACT_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
