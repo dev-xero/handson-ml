9f6496d50cc38647dde27c2c9eab1541b9ed890e
= <ssh.xero@gmail.com>
2025-07-07 21:32:12 +0100

    feat: split training and test by hash fn
    Signed-off-by: = <ssh.xero@gmail.com>

--- ch-02-end-to-end-ml-project/utils/data.py
+++ ch-02-end-to-end-ml-project/utils/data.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from urllib import request
 from pathlib import Path
+from zlib import crc32
 
 HOUSING_PATH = os.path.join("datasets", "housing")
 EXTRACT_PATH = os.path.join(Path(__file__).parents[1], HOUSING_PATH)
@@ -34,3 +35,13 @@ def split_train_test(data, test_ratio):
     # select other half for training
     train_indices = shuffled_indices[test_set_size:]
     return data.iloc[train_indices], data.iloc[test_indices]
+
+
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
