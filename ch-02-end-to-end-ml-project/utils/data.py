9d0d718d5a02a76554816e1621f95db7748ae41f
= <ssh.xero@gmail.com>
2025-07-07 21:21:01 +0100

    feat: split training and test datasets
    Signed-off-by: = <ssh.xero@gmail.com>

--- ch-02-end-to-end-ml-project/utils/data.py
+++ ch-02-end-to-end-ml-project/utils/data.py
@@ -1,6 +1,7 @@
 import os
 import tarfile
 import pandas as pd
+import numpy as np
 
 from urllib import request
 from pathlib import Path
@@ -10,14 +11,26 @@ EXTRACT_PATH = os.path.join(Path(__file__).parents[1], HOUSING_PATH)
 
 
 def download_housing_data(housing_url, housing_path=EXTRACT_PATH):
-    os.makedirs(housing_path, exist_ok=True)
-    tgz_path = os.path.join(housing_path, "housing.tgz")
-    request.urlretrieve(housing_url, tgz_path)
-    housing_tgz = tarfile.open(tgz_path)
-    housing_tgz.extractall(path=housing_path)
-    housing_tgz.close()
+    if not os.path.isdir(EXTRACT_PATH):
+        os.makedirs(housing_path, exist_ok=True)
+        tgz_path = os.path.join(housing_path, "housing.tgz")
+        request.urlretrieve(housing_url, tgz_path)
+        housing_tgz = tarfile.open(tgz_path)
+        housing_tgz.extractall(path=housing_path)
+        housing_tgz.close()
 
 
 def load_housing_data(housing_path=EXTRACT_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
+
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    # assume test set size is N
+    # select first N entries
+    test_indices = shuffled_indices[:test_set_size]
+    # select other half for training
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
